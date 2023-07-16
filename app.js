86b9b929f7c4ba8ff1cdc2d47dd74e4f816e53c0
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-16 15:07:42 +0300

    Add files via upload
    Type Conversion

--- app.js
+++ app.js
@@ -1,8 +1,45 @@
 "use strict";
 // Null
-const n0 = null;
-const n1 = null;
-const n2 = null;
-const n3 = null;
-const n4 = null;
-const n5 = null;
+// "strictNullChecks": false
+/*
+
+const n0: null = null;
+const n1: any = null;
+const n2: string = null;
+const n3: number = null;
+const n4: boolean = null;
+const n5: undefined = null;
+
+interface User {
+    name: string
+}
+
+function getUser(){
+    if(Math.random() > 0.5){
+        return null;
+    } else {
+        return {
+            name: 'Dima'
+        } as User
+    }
+}
+
+const user = getUser();
+if(user) {
+    const n = user.name;
+}
+
+*/
+// Type Conversion
+let a = 5;
+let b = a.toString();
+let c = 'someString';
+let d = parseInt(c);
+let e = new String(a).valueOf();
+const user = {
+    name: 'Dima',
+    email: 'someMail@mail.com',
+    login: 'someLogin'
+};
+const admin = Object.assign(Object.assign({}, user), { role: 1 });
+console.log(admin);
--- app.ts
+++ app.ts
@@ -3,9 +3,77 @@
 // Null
 
 // "strictNullChecks": false
+
+/*
+
 const n0: null = null;
 const n1: any = null;
 const n2: string = null;
 const n3: number = null;
 const n4: boolean = null;
 const n5: undefined = null;
+
+interface User {
+    name: string
+}
+
+function getUser(){
+    if(Math.random() > 0.5){
+        return null;
+    } else {
+        return {
+            name: 'Dima'
+        } as User
+    }
+}
+
+const user = getUser();
+if(user) {
+    const n = user.name;
+}
+
+*/
+
+
+// Type Conversion
+
+let a = 5;
+let b: string = a.toString();
+
+let c = 'someString';
+let d: number = parseInt(c);
+
+let e: string = new String(a).valueOf();
+
+
+
+interface User {
+    name: string;
+    email: string;
+    login: string;
+}
+
+const user: User = {
+    name: 'Dima',
+    email: 'someMail@mail.com',
+    login: 'someLogin'
+}
+
+interface Admin {
+    name: string;
+    role: number;
+}
+
+const admin: Admin = {
+    ...user,
+    role: 1
+}
+
+//console.log(admin);
+
+function userToAdmin(user: User): Admin{
+    return {
+        name: user.name,
+        role: 1
+    }
+}
