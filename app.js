19796a3d5df7019d52a45bb720afc7c4ad6d2640
Am1d1v <124504237+Am1d1v@users.noreply.github.com>
2023-07-16 14:34:50 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+// Null
+const n0 = null;
+const n1 = null;
+const n2 = null;
+const n3 = null;
+const n4 = null;
+const n5 = null;
---
+++ app.ts
@@ -0,0 +1,11 @@
+
+
+// Null
+
+// "strictNullChecks": false
+const n0: null = null;
+const n1: any = null;
+const n2: string = null;
+const n3: number = null;
+const n4: boolean = null;
+const n5: undefined = null;
